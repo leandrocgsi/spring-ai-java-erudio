6570dda65d16a538ce43900539a0b6e479ceef00
leandrocgsi <leandrocgsi@gmail.com>
2025-03-12 07:24:33 -0300

    Chat, Recipe Generation, and Images with Spring AI, OpenAI, and ChatGPT

--- 06_Integrating_SpringAI_OpenAI_FirstProject/spring-ai-erudio/src/main/java/br/com/erudio/controller/GenerativeAIController.java
+++ 06_Integrating_SpringAI_OpenAI_FirstProject/spring-ai-erudio/src/main/java/br/com/erudio/controller/GenerativeAIController.java
@@ -1,20 +1,28 @@
 package br.com.erudio.controller;
 
 import br.com.erudio.service.ChatService;
+import br.com.erudio.service.ImageService;
 import br.com.erudio.service.RecipeService;
+import org.springframework.ai.image.ImageResponse;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 public class GenerativeAIController {
 
     private final ChatService chatService;
     private final RecipeService recipeService;
+    private final ImageService imageService;
 
-    public GenerativeAIController(ChatService chatService, RecipeService recipeService) {
+    public GenerativeAIController(ChatService chatService,
+                                  RecipeService recipeService,
+                                  ImageService imageService) {
         this.chatService = chatService;
         this.recipeService = recipeService;
+        this.imageService = imageService;
     }
 
     @GetMapping("ask-ai")
@@ -33,5 +41,19 @@ public class GenerativeAIController {
                                 @RequestParam(defaultValue = "none") String dietaryRestrictions) {
 
         return recipeService.createRecipe(ingredients, cuisine, dietaryRestrictions);
+    }
+
+    @GetMapping("generate-image")
+    public List<String> generateImages(@RequestParam String prompt,
+                                       @RequestParam(defaultValue = "hd") String quality,
+                                       @RequestParam(defaultValue = "1") Integer n,
+                                       @RequestParam(defaultValue = "1024") Integer height,
+                                       @RequestParam(defaultValue = "1024") Integer width) {
+
+        ImageResponse response = imageService.generateImage(prompt, quality, n, height, width);
+        List<String> imageUrls = response.getResults().stream()
+                .map(result -> result.getOutput().getUrl())
+            .toList();
+        return imageUrls;
     }
 }
